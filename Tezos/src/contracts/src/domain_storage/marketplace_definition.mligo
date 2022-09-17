b9bd52c287b2d3d24e0905797af20b2bf63bf927
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-09-17 08:25:56 +0000

    NFT game page

--- Tezos/src/contracts/src/domain_storage/marketplace_definition.mligo
+++
@@ -1,7 +0,0 @@
-type sale = {
-    token_id: token_id;
-    price: price;
-}
-type lands = (nat, land) big_map
-
-type owners = (address, token_id set) big_map
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter as Router} from 'react-router-dom'
+import './App.css'
 
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
